9fb1b2bfb4df8184600f1d2a363a02ccc3e914cf
Stidz-PC <marszp8888@gmail.com>
2022-03-16 21:34:52 +0100

    Added an old way to configure Bean by xml file + code cleaning

--- ReadMe.txt
+++ ReadMe.txt
@@ -1,3 +1,4 @@
 SPRING SIMPLE APP:
 Spring Bean configuration and Dependency Injection done by Java Annotations -> JavaAnnotationConfig.
-Bean handled in -> MainSpringConfigurationWithJavaAnnotations
+Bean handled in -> MainSpringConfigurationWithJavaAnnotationsgit
+Additional in comments an old way to configure Spring by xml file
--- com/msz/spring/configwithannotations/MainSpringConfigurationWithJavaAnnotations.java
+++ com/msz/spring/configwithannotations/MainSpringConfigurationWithJavaAnnotations.java
@@ -1,22 +1,29 @@
 package com.msz.spring.configwithannotations;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MainSpringConfigurationWithJavaAnnotations {
     public static void main(String[] args) {
 
-        AnnotationConfigApplicationContext context =
-                new AnnotationConfigApplicationContext(JavaAnnotationConfig.class);
+        //old way -> Xml file
+        //ClassPathXmlApplicationContext context =
+        //        new ClassPathXmlApplicationContext("applicationContextOld.xml");
 
+        //new way -> Java annotations
+        AnnotationConfigApplicationContext context =
+        new AnnotationConfigApplicationContext(JavaAnnotationConfig.class);
+
+        //getting Beans
         Message SundayMessage = context.getBean("sundayMessage",Message.class);
         CalendarDay Calendar = context.getBean("calendar",CalendarDay.class);
 
+        //Calling methods on bean
         System.out.println(SundayMessage.getMessage());
         System.out.println(Calendar.getActualDay());
         System.out.println(Calendar.getMessage());
 
-
+        //cleaning
         context.close();
-
     }
 }
